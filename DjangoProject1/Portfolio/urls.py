5d73db5ad36720ee21cdc28d9b39a26b6d21c444
Toogii <dashdava@uw.edu>
2020-08-07 18:39:25 -0700

    Working on project portfolio pages

--- DjangoProject1/Portfolio/urls.py
+++ DjangoProject1/Portfolio/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='home-page'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('contact/', views.portfolio, name='portfolio'),
+    path('portfolio/', views.portfolio, name='portfolio'),
 ]
