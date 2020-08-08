8de0cebddddd6812c652c4e80e7feadc2895c876
Toogii <dashdava@uw.edu>
2020-08-07 17:40:55 -0700

    Added other pages

--- DjangoProject1/Portfolio/urls.py
+++ DjangoProject1/Portfolio/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home-page'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('contact/', views.portfolio, name='portfolio'),
 ]
--- DjangoProject1/Portfolio/views.py
+++ DjangoProject1/Portfolio/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
+
+def portfolio(request):
+    return render(request, "portfolio.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def about(request):
+    return render(request, "about.html")
